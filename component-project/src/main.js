12aa1d5fd9a3e1b397d3cfe9e3958e1ff2073cb3
BlockGem <kifeanyi216@gmail.com>
2023-08-17 01:37:45 +0100

    style base-button

--- component-project/src/main.js
+++ component-project/src/main.js
@@ -2,10 +2,11 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import BaseCard from './components/UI/BaseCard.vue';
-import TheHeader from './components/layouts/TheHeader.vue';
+import BaseButton from './components/UI/BaseButton.vue';
 
 const app = createApp(App);
 
 app.component('base-card', BaseCard);
-app.component('the-header', TheHeader);
+app.component('base-button', BaseButton);
+
 app.mount('#app');
