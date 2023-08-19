0172a5fb0878dce77f330d5261e7f10c6dbfecd4
BlockGem <kifeanyi216@gmail.com>
2023-08-19 17:50:07 +0100

    Handling Technical, Browser/Side Error

--- http-vue-01/src/components/survey/UserExperiences.vue
+++ http-vue-01/src/components/survey/UserExperiences.vue
@@ -8,10 +8,13 @@
         >
       </div>
       <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!Loading && error">
+        {{ error }}
+      </p>
       <p v-else-if="!isLoading && (!results || results.length === 0)">
         No stored experiences found. Start adding some survey results
       </p>
-      <ul v-else-if="!isLoading && results && results.length > 0">
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -34,11 +37,13 @@ export default {
     return {
       results: [],
       isLoading: false,
+      error: null,
     };
   },
   methods: {
     loadExperiences() {
       this.isLoading = true;
+      this.error = null;
       fetch(
         'https://vue-http-pro-dc119-default-rtdb.firebaseio.com/surveys.json'
       )
@@ -58,6 +63,11 @@ export default {
             });
           }
           this.results = results;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.isLoading = false;
+          this.error = 'Failed to fetch data - please try again later';
         });
     },
   },
