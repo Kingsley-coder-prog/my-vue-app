c6bbb8bf2500d87bdf2d1adf3630c1b5029bd02f
BlockGem <kifeanyi216@gmail.com>
2023-08-20 15:49:56 +0100

    Redirect & 'Catch All' Routes

--- vue-routing/src/main.js
+++ vue-routing/src/main.js
@@ -5,13 +5,19 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
+      path: '/',
+      redirect: '/teams',
+    },
+    {
       path: '/teams',
       component: TeamsList, // our-domain.com/teams => ...
+      //   alias: '/',
     },
     {
       path: '/users',
@@ -21,6 +27,10 @@ const router = createRouter({
       path: '/teams/:teamId',
       component: TeamMembers,
       props: true,
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound,
     },
   ],
   linkActiveClass: 'active',
